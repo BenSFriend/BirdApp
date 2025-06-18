ad068fea524e03dbc48bb1b19f02813b35626370
michaelamary7 <michaela713@me.com>
2025-06-18 18:14:50 -0400

    added code to BirdCard.jsx and .css

---
+++ client/bird-app/src/components/BirdCard/BirdCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './BirdCard.css';
+
+const BirdCard = ({ 
+  bird, 
+  scale, 
+  position, 
+  isHovered, 
+  onMouseEnter, 
+  onMouseLeave, 
+  onClick 
+}) => {
+  return (
+    <div
+      className="bird-card-container"
+      style={{
+        left: `${position.left}px`,
+        top: `${position.top}px`,
+        transform: `translate(-50%, -50%) scale(${scale})`,
+        zIndex: isHovered ? 15 : scale > 1 ? 10 : 5,
+      }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
+    >
+      <div className="bird-card">
+        <div className="bird-image">
+          <img
+            src={bird.image}
+            alt={bird.name}
+          />
+        </div>
+        
+        {isHovered && (
+          <div className="bird-tooltip">
+            <div className="bird-name">{bird.name}</div>
+            <div className="bird-species">{bird.species}</div>
+            <div className="tooltip-arrow"></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BirdCard;
